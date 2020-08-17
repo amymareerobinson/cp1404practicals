7dd970c80ea847f8bf6806aa12bd00e2b965adf0
Amy Robinson <am88robinson@gmail.com>
2020-08-17 20:59:37 +1000

    Refactor password_check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -6,12 +6,31 @@ Program - Password Check
 
 MINIMUM_LENGTH = 6
 
-password = input("Enter password: ")
 
-while len(password) < MINIMUM_LENGTH:
-    print("Invalid password length")
+def main():
+    """check for valid password and display length in stars"""
+
+    password = get_password()
+
+    number_of_stars = count_characters(password)
+
+    print(number_of_stars * "*", end="")
+
+
+def get_password():
+    """get a valid password"""
     password = input("Enter password: ")
 
-password_length = len(password)
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid password length")
+        password = input("Enter password: ")
+    return password
 
-print("*" * password_length, end='')
+
+def count_characters(string_variable):
+    """count the number of characters in a string_variable"""
+    count_character = len(string_variable)
+    return count_character
+
+
+main()
