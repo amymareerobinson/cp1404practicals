e956a9bdb9fbca47bb4220eb448827f2ee9b00ac
Amy Robinson <am88robinson@gmail.com>
2020-08-20 17:03:21 +1000

    Refactor broken_score program to use a function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -4,32 +4,37 @@ Student Name: Amy Robinson
 Program - Broken Score
 """
 
-# TODO: Fix this!
+import random
 
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#         print("Excellent")
-# if score < 50:
-#     print("Bad")
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
 
 
-score = float(input("Enter score: "))
+def main():
+    """generate scores and print the results"""
 
-if score < 0 or score > 100:
-    print("Invalid score")
+    # Takes in a user score and prints the result
+    score = int(input("Enter score: "))
+    score_result = display_score_result(score)
+    print(f"{score:2} is {score_result}")
 
-elif score >= 90:
-    print("Excellent")
+    # Generates a random score and prints the result
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+    random_score_result = display_score_result(random_score)
+    print(f"{random_score:2} is {random_score_result}")
 
-elif score >= 50:
-    print("Passable")
 
-else:
-    print("Bad")
+def display_score_result(score):
+    """display a score result from variable score"""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
