291d4fe5fc4f3041760c93a3ab3845c19e5583ed
Amy Robinson <am88robinson@gmail.com>
2020-08-28 12:28:31 +1000

    Update to apply to pylint standards

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -19,7 +19,9 @@ print(f"The first number is {numbers[0]}\n"
 
 
 # 2. Woefully inadequate security checker
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 username = input("Enter Username: ")
 
