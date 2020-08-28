c74971d844ce996d03321c7a6e89166f296d4a70
Amy Robinson <am88robinson@gmail.com>
2020-08-28 12:39:33 +1000

    Update answered errors per lecturers' feedback

--- prac_02/randoms.py
+++ prac_02/randoms.py
@@ -8,7 +8,7 @@ Program - Randoms
 import random
 
 print(random.randint(5, 20))  # generates a random integer between 5 and 20
-print(random.randrange(3, 10, 2))  # generates a random odd integer between 3 and 10
+print(random.randrange(3, 10, 2))  # generates a random odd integer between 3 and 9
 print(random.uniform(2.5, 5.5))  # generates a random decimal between 2.5 and 5.5
 
 """
@@ -20,13 +20,13 @@ largest number = 20
 What did you see on line 2? 7
 What was the smallest number you could have seen, what was the largest?
 smallest = 3
-largest = 10
+largest = 9
 Could line 2 have produced a 4? No
 
 What did you see on line 3? 4.136196483497342
 What was the smallest number you could have seen, what was the largest?
 smallest = 2.5
-largest = 10
+largest = 5.5
 """
 
 # Write code, not a comment, to produce a random number between 1 and 100 inclusive.
