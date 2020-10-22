dd467f1ed91527e57d2f6f55eef4704e6a399830
Amy Robinson <am88robinson@gmail.com>
2020-10-22 19:07:54 +1000

    Update TODO comments

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -10,7 +10,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return ' '.join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -23,7 +23,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -33,7 +33,7 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
+    # 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -42,23 +42,27 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
+    assert test_car.fuel == 0
     test_car = Car(fuel=10)
+
+    assert not test_car.fuel == 0
+    assert test_car.fuel == 10
 
 
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
+# 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+# 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
+# p5. Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
@@ -67,3 +71,19 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+
+
+def format_a_sentence(phrase):
+    """
+    Format a phrase as a sentence.
+    >>> format_a_sentence('hello')
+    'Hello.'
+    >>> format_a_sentence('It is an ex parrot.')
+    'It is an ex parrot.'
+    >>> format_a_sentence('capital finishing with a full stop')
+    'Capital finishing with a full stop.'
+    """
+    if not phrase[0].isupper() and phrase[-1] != '.':
+        return phrase[0].upper() + phrase[1:] + '.'
+    else:
+        return phrase
